f95ff52bf1ab3ff4c7d722593d00926df3198497
gbmarc1 <marcantoinebelanger.gbmarc@gmail.com>
2019-03-17 17:31:01 -0400

    initial tree modifications as per https://github.com/openai/gym/tree/master/gym/envs

---
+++ gym_cribbage/envs/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# @Author: Marc-Antoine
+# @Date:   2019-03-17 17:18:42
+# @Last Modified by:   Marc-Antoine Belanger
+# @Last Modified time: 2019-03-17 17:20:31
+
+from gym_cribbage.envs.cribbage_env import CribbageEnv
---
+++ setup.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# @Author: Marc-Antoine
+# @Date:   2019-03-17 17:18:42
+# @Last Modified by:   Marc-Antoine Belanger
+# @Last Modified time: 2019-03-17 17:20:31
+
+from setuptools import setup
+
+setup(name='gym_cribbage',
+      version='0.0.1',
+      install_requires=['gym']  # And any other dependencies cribbage needs
+)
