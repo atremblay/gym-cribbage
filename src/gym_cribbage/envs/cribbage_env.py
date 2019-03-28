5980c9f639333acb57072c99907d8e6fd8441679
Alexis Tremblay <alexis.tremblay@gmail.com>
2019-03-28 06:59:15 -0400

    Use compact state instead of short state. Short state was not gonna be used probably

--- src/gym_cribbage/envs/cribbage_env.py
+++ src/gym_cribbage/envs/cribbage_env.py
@@ -69,12 +69,13 @@ class Card(object):
         return s
 
     @property
-    def short_state(self):
+    def compact_state(self):
         # [0:3] encode suit, [4:16] encode rank
-        s = np.zeros(4 + 13)
-        s[SUITS.index(self.suit)] = 1
-        s[RANKS.index(self.rank) + 4] = 1
-        return s
+        suit = np.zeros(4)
+        rank = np.zeros(13)
+        suit[SUITS.index(self.suit)] = 1
+        rank[RANKS.index(self.rank)] = 1
+        return suit, rank
 
     def __repr__(self):
         return "{}{}".format(self.rank, self.suit)
@@ -109,6 +110,20 @@ class Deck(object):
 
     def deal(self):
         return self.cards.pop(0)
+
+    def remove(self, card):
+        new_cards = []
+        for c in self.cards:
+            if c != card:
+                new_cards.append(c)
+        new_deck = Deck()
+        new_deck.cards = new_cards
+        return new_deck
+
+    def remove_(self, card):
+        for i, c in enumerate(self.cards):
+            if c == card:
+                self.cards.pop(i)
 
     def __len__(self):
         return len(self.cards)
@@ -149,15 +164,22 @@ class Stack(object):
         return s
 
     @property
-    def short_state(self):
+    def compact_state(self):
         """
         Possibly for state aggregation.
         """
         # [0:3] encode suit, [4:16] encode rank
-        s = np.zeros(4 + 13)
-        for card in self.cards:
-            s += card.state
-        return s
+        suit = np.zeros((4, len(self)), dtype=np.float32)
+        rank = np.zeros((13, len(self)), dtype=np.float32)
+        for i, card in enumerate(self.cards):
+            s, r = card.compact_state
+            suit[:, i] = s
+            rank[:, i] = r
+
+        argsort = np.argsort([c.rank_value for c in self])
+        suit = suit[:, argsort]
+        rank = rank[:, argsort]
+        return suit, rank
 
     def add(self, card):
         if not isinstance(card, Card):
